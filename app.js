f3bb6a4ebc903b437e22bb1aebdea445ceaf54e4
noobmaester <joshstilezgmail.com>
2023-07-19 00:13:15 +0200

    button that triggers a prompt method

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const container = document.querySelector(".container");
 
 //creating a button
 const button = document.createElement("button");
-const header = document.querySelector(".header")
+const header = document.querySelector(".header");
 button.innerText = "Click Me";
 button.id = "btn";
 header.appendChild(button);
 
 for (let i = 1; i < 257; i++){
     const grid = document.createElement("div");
-    grid.id = "grid";
+    grid.classList.add("grid");
     container.appendChild(grid);
 
     grid.addEventListener("mouseenter", (event) => {
@@ -18,8 +18,28 @@ for (let i = 1; i < 257; i++){
     })
 }
 
+const container2 = document.querySelector(".container2");
 
+button.addEventListener("click", (event) => {
+  let popup = prompt("How many squares per Side for the new grids?");
 
+  let numberOfSquares = popup * popup;
 
+  if (popup <= 100) {
+    for (let i = 0; i < numberOfSquares; i++) {
+      const grid2 = document.createElement("div");
+      grid2.classList.add("grid");
+      container2.appendChild(grid2);
+      console.log(numberOfSquares);
 
+      container.style.display = "none";
 
+      
+      grid2.addEventListener("mouseenter", (event) => {
+        grid2.style.backgroundColor = "gold";
+      });
+    }
+  } else {
+    alert("100 is the Maximum!");
+  }
+});
