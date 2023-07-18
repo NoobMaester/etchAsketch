40510d3cd5f8408a6aa7d5a515d203ea556e52a3
noobmaester <joshstilezgmail.com>
2023-07-18 08:14:15 +0200

    button

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //creating a parent element constant
 const container = document.querySelector(".container");
 
+//creating a button
+const button = document.createElement("button");
+const header = document.querySelector(".header")
+button.innerText = "Click Me";
+button.id = "btn";
+header.appendChild(button);
 
 for (let i = 1; i < 257; i++){
     const grid = document.createElement("div");
@@ -11,4 +17,9 @@ for (let i = 1; i < 257; i++){
         grid.style.backgroundColor = "gold";
     })
 }
+
+
+
+
+
 
